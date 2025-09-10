a3bb842f4b7bc5874b4f1dab2c0f4b811c8e2b3b
destronfgc <jalenimitchell99@gmail.com>
2025-09-10 02:15:05 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// This configuration is specific to your Firebase project.
+// It allows your overlay and control panel to connect to the correct database.
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBTeMGX5wgSubD_8S3_1b7Ggn-Xn3PfHUI",
+  authDomain: "stillwater-overlay.firebaseapp.com",
+  projectId: "stillwater-overlay",
+  // The line below has been corrected.
+  storageBucket: "stillwater-overlay.firebasestorage.app",
+  messagingSenderId: "207060991222",
+  appId: "1:207060991222:web:ed97feecfaa98f691380d7",
+  measurementId: "G-SKPQ6X91NP"
+};
+
