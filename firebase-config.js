f9090d28186a7ea44ebae5e29764e7390e477f3e
destronfgc <jalenimitchell99@gmail.com>
2025-09-10 02:36:28 -0500

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,10 +2,10 @@
 // It allows your overlay and control panel to connect to the correct database.
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBTeMGX5wgSubD_8S3_1b7Ggn-Xn3PfHUI",
+  // PASTE YOUR NEW KEY ON THE LINE BELOW
+  apiKey: "AIzaSyBTeMGx5wg5ubD_8s3_1b7Ggn-Xn3PfhUI",
   authDomain: "stillwater-overlay.firebaseapp.com",
   projectId: "stillwater-overlay",
-  // The line below has been corrected.
   storageBucket: "stillwater-overlay.firebasestorage.app",
   messagingSenderId: "207060991222",
   appId: "1:207060991222:web:ed97feecfaa98f691380d7",
